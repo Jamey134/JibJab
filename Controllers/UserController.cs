fa0f9aa6db0ecf8e1694f49234328b05ad1b43a4
James Watson <jameywatson134@gmail.com>
2023-09-02 12:56:51 -0700

    update

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -53,7 +53,7 @@ public class UserController : Controller
         db.SaveChanges();
         HttpContext.Session.SetInt32("UUID", newUser.UserId);
 
-        return RedirectToAction("Index", "Wedding"); // Make sure to change for the exam("Method Action from controller", "Name of controller where the method is from")
+        return RedirectToAction("Index", "blog"); // Make sure to change for the exam("Method Action from controller", "Name of controller where the method is from")
 
     }
     //-----Login-----
@@ -84,7 +84,7 @@ public class UserController : Controller
             {
                 //HANDLE SUCCESS (THIS SHOULD ROUTE TO AN INTERNAL PAGE)
                 HttpContext.Session.SetInt32("UUID", userInDb.UserId);
-                return RedirectToAction("Index", "Wedding"); // Make sure to change for the exam("Method Action from controller", "Name of controller where the method is from")
+                return RedirectToAction("Index", "blog"); // Make sure to change for the exam("Method Action from controller", "Name of controller where the method is from")
             }
 
         }
